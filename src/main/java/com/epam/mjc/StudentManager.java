82e294b3c6018e0facb1dffc11e8d570a7ee97c6
Matvey Stetcenko <matveyvs1987@gmail.com>
2023-01-14 13:33:52 +0400

    Problem solving

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -1,21 +1,27 @@
 package com.epam.mjc;
 
-
 public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+  public Student find(long studentID) throws CustomException {
+      if (studentID <= 10){
+        return Student.getValueOf(studentID);
+      } else {
+        throw new CustomException("Could not find student with ID " + studentID);
+      }
   }
 
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
 
     for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+      try {
+        Student student = manager.find(IDs[i]);
+        System.out.println("Student name " + student.getName());
+      }catch (CustomException ex){
+        System.err.print(ex);
+      }
     }
-
   }
 }
